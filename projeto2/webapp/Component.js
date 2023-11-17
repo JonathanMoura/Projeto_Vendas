30086f3159da13cda9c686444d2610e89c7f0ebd
Jonathan Moura <jonathan_moura@outlook.com.br>
2023-11-17 23:15:32 +0000

    Initial Commit @11/17/2023, 11:15:32 PM

---
+++ projeto2/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("projeto2.projeto2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
